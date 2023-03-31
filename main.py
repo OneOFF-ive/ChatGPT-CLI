c20709c73ed0a3d5ea9f3eb8ed304e467f6bbd9d
Wu <2321141691@qq.com>
2023-03-31 20:09:41 +0800

    实现了简单的CompletionChat接口

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import os
 import openai
 
-openai.organization = "org-nQ2su19ado3KvvI9HE5vAZHO"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-completions = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "Hello!"}],
-    temperature=1,
-    n=1,
-    stream=False,
-    stop='\n',
-    max_tokens=2048,
-    presence_penalty=0,
-    frequency_penalty=0,
-)
-message = completions.choices[0]["message"]["content"]
-print(message)
 
+def generateCompletion(msg):
+    return openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=msg,
+        temperature=1,
+        n=1,
+        stream=False,
+        stop='\n',
+        max_tokens=2048,
+        presence_penalty=0,
+        frequency_penalty=0,
+    )
+
+
+def getResult(completions):
+    return completions.choices[0]["message"]["content"]
+
+
+def main():
+    openai.organization = "org-nQ2su19ado3KvvI9HE5vAZHO"
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    messages: list[dict] = [{"role": "system", "content": "You are a helpful assistant."}]
+    while True:
+        prompt: str = input()
+        message: dict[str: str] = {"role": "user", "content": prompt}
+        messages.append(message)
+
+        result = getResult(generateCompletion(messages))
+        messages.append({"role": "assistant", "content": result})
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
