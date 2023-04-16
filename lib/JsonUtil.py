9beb5dc67084676dc81d73c1a2e10c1529768812
Wu <2321141691@qq.com>
2023-04-16 23:22:12 +0800

    重构配置系统

---
+++ lib/JsonUtil.py
@@ -0,0 +1,30 @@
+import os
+import json
+from config import *
+
+
+class JsonUtil:
+    @staticmethod
+    def json2Config(file_dir: str, file_name: str):
+        # 获取当前文件所在目录的上级目录，也就是项目根目录
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        root_path = os.path.abspath(os.path.join(root_path, '..'))
+
+        # 构造json文件路径
+        conf_path = os.path.join(root_path, file_dir, file_name)
+
+        # 读取json文件内容
+        with open(conf_path, 'r') as f:
+            conf_data = json.load(f)
+
+        # 将json数据序列化
+        completionConfig = CompletionConfig(**conf_data.get("CompletionConfig"))
+        imageConfig = ImageConfig(**conf_data.get("ImageConfig"))
+        transcriptionsConfig = TranscriptionsConfig(**conf_data.get("TranscriptionsConfig"))
+
+        return completionConfig, imageConfig, transcriptionsConfig
+
+
+__all__ = [
+    "JsonUtil"
+]
---
+++ lib/config.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class CompletionConfig:
+    system_prompt: str
+    model: str
+    temperature: int
+    n: int
+    stream: bool
+    stop: str
+    max_tokens: int
+    presence_penalty: int
+    frequency_penalty: int
+
+
+@dataclass
+class ImageConfig:
+    n: int
+    size: str
+    response_format: str
+
+
+@dataclass
+class TranscriptionsConfig:
+    model: str
+    response_format: str
+
+
+__all__ = [
+    "CompletionConfig",
+    "ImageConfig",
+    "TranscriptionsConfig"
+]
