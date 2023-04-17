15e1e64ab95da2b1ae2a01c32d84f11ef755d4a1
Wu <2321141691@qq.com>
2023-04-17 17:45:03 +0800

    小型重构命令处理器

--- ClIHandle.py
+++ ClIHandle.py
@@ -1,21 +1,20 @@
-from typing import Dict, Callable
 from Log import Log
 
 
 class ClIHandle:
 
     def __init__(self):
-        self.name2Func: Dict[str, Callable[[], None]] = {}
+        self.name2Func = {}
 
-    def add(self, name: str, func: Callable[[], None]):
+    def add(self, name: str, func):
         self.name2Func[name] = func
 
     def delete(self, name: str):
         return self.name2Func.pop(name)
 
-    def parse(self, name: str):
-        function: Callable[[], None] = self.name2Func.get(name);
+    async def parse(self, name: str, *args):
+        function = self.name2Func.get(name)
         if function is not None:
-            function()
+            await function(*args)
         else:
             Log.error("Option Does Not Exist")
